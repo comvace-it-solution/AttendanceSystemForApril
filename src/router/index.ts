e515ea442d4cf90c8150b94c4b027caae3f12005
Hina Kanda <khina1995ak@gmail.com>
2026-05-21 19:56:58 +0900

    テスト用ファイル削除

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// ログイン画面をインポート
+
 import LoginView from '@/views/LoginView.vue'
-import HomeView from '@/views/HomeView.vue'
+
 
 const router = createRouter({
-
   history: createWebHistory(),
   routes: [
     {
       path: '/login',
       name: 'Login',
       component: LoginView
-    },
-    {
-      path: '/home',
-      name: 'Home',
-      component: HomeView
     },
   ]
 })
